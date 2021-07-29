d77f9fffe7d010d1cc01ca98272f60247e8a799e
Youssef Attia <16466991+youssef-attia@users.noreply.github.com>
2021-07-29 13:20:21 -0400

    add peter the tortoise

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -12,6 +12,12 @@ funAnimals.push({
     reason: "four legs worth of energy at the cost of three"
 })
 
+funAnimals.push({
+    name: "Peter",
+    type: "tortoise",
+    reason: "back yard grass eater"
+})
+
 for (let animal of funAnimals) {
     console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
 }
