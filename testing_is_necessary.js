cc72696c3512113de0010cd5e0180d4b5d02573b
youssef-attia <youssef.attia.ya@gmail.com>
2021-07-29 17:22:51 +0000

    replaced reason with why

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -3,15 +3,15 @@ const funAnimals = []
 funAnimals.push({
     name: "Hygge",
     type: "cat",
-    reason: "agressive snuggler"
+    why: "agressive snuggler"
 })
 
 funAnimals.push({
     name: "Trecito",
     type: "dog",
-    reason: "four legs worth of energy at the cost of three"
+    why: "four legs worth of energy at the cost of three"
 })
 
 for (let animal of funAnimals) {
-    console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
+    console.log(`${animal.name} the ${animal.type} is fun.  Why, you ask? ${animal.why}`)
 }
